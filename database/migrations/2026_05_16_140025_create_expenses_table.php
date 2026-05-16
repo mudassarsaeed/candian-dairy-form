7dfa67857729d1632c59db8034a2eff32f955cdb
Mudassar Saeed <saeed.mudassar@gmail.com>
2026-05-16 19:47:31 +0500

    skip expenses migration if table exists

--- database/migrations/2026_05_16_140025_create_expenses_table.php
+++ database/migrations/2026_05_16_140025_create_expenses_table.php
@@ -11,19 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (!Schema::hasTable('expenses')) {
         Schema::create('expenses', function (Blueprint $table) {
-        $table->id();
-        $table->string('cat_id');
-        $table->date('order_date');
-        $table->date('arrival_date');
-        $table->date('ending_date');
-        $table->string('receipt')->nullable();
-        $table->string('unit');
-        $table->string('number_bags_bails')->nullable();
-        $table->decimal('unit_price', 10, 2);
-        $table->decimal('total', 10, 2);
-        $table->timestamps();
-    });
+            $table->id();
+            $table->string('cat_id');
+            $table->date('order_date');
+            $table->date('arrival_date');
+            $table->date('ending_date');
+            $table->string('receipt')->nullable();
+            $table->string('unit');
+            $table->string('number_bags_bails')->nullable();
+            $table->decimal('unit_price', 10, 2);
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+          });
+        }
     }
 
     /**
